fff00f26a194e6c84209c6e0789a40f6d776e667
asiopongco <asiopongco@gmail.com>
2018-06-29 15:06:34 -0700

    Created a list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<p> yeeeeer </p>, document.getElementById('root'));
+let dummyData = ["Take a shit", "Eat", "Sleep"];
+
+class Todo extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <ul>
+        {this.props.todos.map((todo) => <li><button>X</button>{todo}</li>)}
+      </ul>
+    )
+  }
+}
+
+class TodoList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<Todo todos={dummyData} />);
+  }
+}
+
+
+ReactDOM.render(<TodoList todos={dummyData}/>, document.getElementById('root'));
